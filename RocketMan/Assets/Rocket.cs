bef9c0fbedf050c93e3e671218f2ffe118e07927
DESKTOP-LR999R6\eruxt <eruxton@outlook.com>
2019-01-11 20:50:45 -0500

    rocket movement

---
+++ RocketMan/Assets/Rocket.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rocketBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rocketBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rocketBody.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.left);
+        }else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.right);
+        }
+    }
+}
